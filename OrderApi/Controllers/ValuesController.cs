cb349e3a1eea46fa5c9d156c1df27ad42e2e8bf6
qiyunkong <2572922513@qq.com>
2021-05-07 09:04:13 +0800

    添加项目文件。

---
+++ OrderApi/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApi.Controllers
+{
+    //路由设置
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        //GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new String[] { "value1", "value2" };
+        }
+
+        //GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id) 
+        {
+            return "valule";
+        }
+
+        //POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        { 
+
+        }
+
+        //PUT api/values
+        [HttpPut("id")]
+        public void Put(int id, [FromBody] string value)
+        { 
+            
+        }
+
+        //DELETE api/value/5
+        [HttpDelete]
+        public void Delete(int id)
+        { 
+            
+        }
+
+
+    }
+}
---
+++ OrderApi/README.md
@@ -0,0 +1,16 @@
+点餐 WEBAPI
+
+项目搭建 
+https://www.cnblogs.com/LYF1997/p/11473967.html
+
+配置Swagger	4.0.1 版本
+https://www.cnblogs.com/yanglang/p/9237337.html
+
+Swagger 采坑初 4.0 以上版本  加上RESTful规范使用
+https://www.yuque.com/docs/share/f7953a48-4eb2-41ca-b08d-735af5b2a53a
+
+
+
+
+项目规范：
+	RESTful 架构详解  https://www.runoob.com/w3cnote/restful-architecture.html
